d192a893a255bdfcdba784f860d28be02619714f
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2017-12-21 15:09:12 +1100

    Initial working version. Opted for flat structure with complex data structure over classes, which would require translating back and forwards to YAML compatible complex data structure.

---
+++ yamanifest/hashing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Copyright 2017 ARC Centre of Excellence for Climate Systems Science
+
+author: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import print_function, absolute_import
+
+import hashlib
+import io
+import nchash
+
+length=io.DEFAULT_BUFFER_SIZE
+
+# List of supported hashes and the ordering used to determine relative expense of
+# calculation
+supported_hashes = ['nchash', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
+
+def hash(path, hashfn):
+    """ A simple wrapper that inspects the hashing function and intercepts
+    calls to nchash so they are processed in a special way.
+
+    TODO: make plugins that allow this transparently
+    """
+
+    if hashfn == 'nchash':
+        m = NCDataHash(path)
+        return m.gethash()
+    else:
+        # from https://stackoverflow.com/a/40961519
+        m = hashlib.new(hashfn)
+        with io.open(path, mode="rb") as fd:
+            for chunk in iter(lambda: fd.read(length), b''):
+                m.update(chunk)
+        return m.hexdigest()
+
---
+++ yamanifest/manifest.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+"""
+Copyright 2017 ARC Centre of Excellence for Climate Systems Science
+
+author: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import print_function, absolute_import
+
+import os
+import yaml
+# from .item import ManifestItem
+from .hashing import hash, supported_hashes
+
+class HashExists(Exception):
+    """Trying to add a hashed value when one already exists"""
+
+class HashNonexistent(Exception):
+    """Trying to check a hashed value when there is none"""
+
+class Manifest(object):
+    """A manifest object
+
+    Attributes:
+        path: a Path object for the manifest file
+        data: an dictionary of manifest items
+    """
+
+    def __init__(self, path, **kwargs):
+        """
+        Return a Manifest object
+        """
+        self.path = path
+        self.data = { 'files': {} }
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+    def load(self):
+        """
+        Load manifest from YAML file
+        """
+        with open(self.path, 'r') as file:
+            self.data = yaml.load(file)
+
+    def dump(self):
+        """
+        Dump manifest from YAML file
+        """
+        with open(self.path, 'w') as file:
+            file.write(yaml.dump(self.data, default_flow_style=False))
+
+    def del_item(self, filepath):
+        """
+        Delete item for filepath in manifest
+        """
+        del(self.data['files'][filepath])
+
+    def add_item(self, filepath, hashfn, force=False):
+        """
+        Add hash value for a filepath given a hashing function (hashfn).
+        If there is already a hash value only overwrite if force=True,
+        otherwise raise exception
+        """
+
+        if filepath in self.data['files']:
+            hashes = self.data['files'][filepath]["hashes"]
+        else:
+            self.data['files'][filepath] = {}
+            self.data['files'][filepath]["hashes"] = {}
+            hashes = self.data['files'][filepath]["hashes"]
+
+        # Save the latest full system path 
+        self.data['files'][filepath]['fullpath'] = os.path.realpath(filepath)
+
+        if type(hashfn) is str:
+            functions = [hasfn,]
+        else:
+            functions = hashfn
+            
+        for fn in functions:
+
+            hashval = hash(filepath, fn)
+
+            if fn in hashes:
+                if hashes[fn] != hashval:
+                    if not force:
+                        raise HashExists('Tried to add {} to {}'.format(fn,filepath))
+
+            # Set new value for this has function
+            hashes[fn] = hashval
+        
+    def check_item(self, filepath, hashfns=None):
+        """
+        Check hash value for a filepath given a hashing function (hashfn)
+        matches stored hash value
+        """
+
+        if filepath in self.data['files']:
+            hashes = self.data['files'][filepath]["hashes"]
+        else:
+            raise HashNonexistent('{} does not exist in manifest'.format(filepath))
+
+        if hashfn == None:
+            functions = hashes.keys
+        elif type(hashfn) is str:
+            functions = [hasfn,]
+        else:
+            functions = hashfn
+            
+        for fn in functions:
+            # Short circuit and return false. Might want to
+            # save these values if they're expensive to generate
+            if hash(filepath, hashfn) != hashes[hashfn]:
+                return False
+
+        return True
+
+        
+            
+            
