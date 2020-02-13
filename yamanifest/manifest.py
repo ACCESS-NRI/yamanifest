734eb24e4f3c0946d1e1b978593aa5cc63cc07e2
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-02-13 12:42:46 +1100

    Specify loader to avoid deprecation warning

--- yamanifest/manifest.py
+++ yamanifest/manifest.py
@@ -96,7 +96,7 @@ class Manifest(object):
         """
         try:
             with open(self.path, 'r') as file:
-                self.header, self.data = yaml.load_all(file)
+                self.header, self.data = yaml.load_all(file, Loader=yaml.FullLoader)
             if "format" not in self.header:
                 raise ValueError('Not yamanifest format')
             if self.header["format"] != 'yamanifest':
